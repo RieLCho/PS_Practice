e18ab60426c30d3f0c39e7d995feabee230238e1
RieL <choyj1222@gmail.com>
2021-08-01 19:24:39 +0900

    Update 118p_게임개발.py

--- Baekjoon/이코테/118p_게임개발.py
+++ Baekjoon/이코테/118p_게임개발.py
@@ -1,1 +1,40 @@
-print('test')
+n, m = map(int, input().split())
+d = [[0] * m for _ in range(n)]
+x, y, direction = map(int, input().split())
+d[x][y] = 1
+gameMap = [list(map(int, input().split())) for _ in range(m)]
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if d[nx][ny] == 0 and gameMap[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        if gameMap[nx][ny] == 0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+print(count)
